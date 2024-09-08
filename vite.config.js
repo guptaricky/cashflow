e7b9249364bf275f1aa23b270c677dfa904e4712
mayankgupta-5000049 <mayank.gupta@censanext.com>
2024-09-08 15:19:35 +0530

    tooltip click redirect to conversion

--- vite.config.js
+++ vite.config.js
@@ -14,12 +14,13 @@ export default defineConfig({
                 'resources/js/app.js',
 
                 // theme css
-                'resources/assets/libs/jsvectormap/css/jsvectormap.min.css',
+                
                 'resources/assets/css/bootstrap.min.css',
                 'resources/assets/css/icons.min.css',
                 'resources/assets/css/app.min.css',
                 'resources/assets/css/custom.min.css',
                 'resources/assets/libs/gridjs/theme/mermaid.min.css',
+                // 'resources/assets/libs/jsvectormap/css/jsvectormap.min.css',
                 
                 'resources/assets/libs/bootstrap/js/bootstrap.bundle.min.js',
                 'resources/assets/libs/simplebar/simplebar.min.js',
@@ -27,15 +28,15 @@ export default defineConfig({
                 'resources/assets/libs/feather-icons/feather.min.js',
                 'resources/assets/js/pages/plugins/lord-icon-2.1.0.js',
                 'resources/assets/js/plugins.js',
-                'resources/assets/libs/apexcharts/apexcharts.min.js',
-                'resources/assets/libs/jsvectormap/js/jsvectormap.min.js',
-                'resources/assets/libs/jsvectormap/maps/world-merc.js',
-                'resources/assets/js/pages/dashboard-analytics.init.js',
-                'resources/assets/js/app.js',
+                // 'resources/assets/libs/apexcharts/apexcharts.min.js',
+                // 'resources/assets/libs/jsvectormap/js/jsvectormap.min.js',
+                // 'resources/assets/libs/jsvectormap/maps/world-merc.js',
+                // 'resources/assets/js/pages/dashboard-analytics.init.js',
                 'resources/assets/libs/prismjs/prism.js',
-                'resources/assets/js/pages/notifications.init.js',
-                'resources/assets/libs/gridjs/gridjs.umd.js',
-                'resources/assets/js/pages/gridjs.init.js'
+                'resources/assets/js/app.js',
+                // 'resources/assets/js/pages/notifications.init.js',
+                // 'resources/assets/libs/gridjs/gridjs.umd.js',
+                // 'resources/assets/js/pages/gridjs.init.js'
 
             ],
             refresh: true,
